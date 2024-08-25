e14295c2e413cf8310cde7274691048f5ddc525a
Your Name <priyavb@gmail.com>
2024-08-25 16:46:16 -0700

    added datatype length

--- schema.sql
+++ schema.sql
@@ -1,26 +1,26 @@
 create table titles(
-	title_id varchar not null,
-    title varchar not null,
+	title_id varchar(20) not null,
+    title varchar(40) not null,
 	primary key(title_id)
 );
-create table employees(
+create table employees(	
 	emp_no int not null,
-    emp_title_id varchar not null,
+    emp_title_id varchar(20) not null ,
 	birth_date DATE not null,
-	first_name varchar not null,
-	last_name varchar not null,
-	sex varchar not null,
+	first_name varchar(50) not null,
+	last_name varchar(50) not null,
+	sex varchar(20) not null,
 	hire_date DATE not null,
 	foreign key(emp_title_id) references titles (title_id),
 	primary key(emp_no)
 ); 	
 create table departments(
-	dept_no varchar not null,
-    dept_name varchar not null,
+	dept_no varchar(20) not null,
+    dept_name varchar(40) not null,
 	primary key(dept_no)
 );
 create table dept_manager(
-	dept_no varchar not null,
+	dept_no varchar(20) not null,
     emp_no int not null,
 	foreign key(emp_no) references employees (emp_no),
     foreign key(dept_no)references departments(dept_no),
@@ -28,7 +28,7 @@ create table dept_manager(
 );
 create table dept_emp(
 	emp_no int not null,
-	dept_no varchar not null,
+	dept_no varchar(20) not null,
 	foreign key(emp_no) references employees (emp_no),
     foreign key(dept_no)references departments(dept_no)
 );
